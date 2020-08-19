5facf72f837e30088d587d69640eb8445a45f906
Charlie B <>
2020-08-19 07:51:23 +0100

    add separate css and js files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function alertName() {
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var resp = `${fname} ${lname} executed JavaScript successfully`;
+    alert(resp)
+    }
+
