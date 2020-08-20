53e46dd73ca32d2150956231cfe1fc9871410df7
Charlie B <>
2020-08-20 13:45:59 +0100

    rename function, add additional error checking

--- js/script.js
+++ js/script.js
@@ -8,13 +8,31 @@ function calculateBMR() {
     var weight_loss ;
     var weight_gain ;
     var resp ;
+    var activity_dict = {1 : 1.2,
+                         2 : 1.375,
+                         3 : 1.55,
+                         4 : 1.725,
+                         5 : 1.9}
+
+    if (age == "" || weight == "" || height == "" || activity == "") {
+        alert("Please complete all fields");
+        return false;
+    }
 
     if (gender == "m") {
-        bmr = Math.round(66.47 + (6.24 * weight) + (12.7 * height) - (6.755 * age));
+        gender = "Male"
+        bmr = Math.round((66.47 + (6.24 * weight) + (12.7 * height) - (6.755 * age))* activity_dict[activity]);
       } 
+
     else {
-        bmr = Math.round(655.1 + (4.35 * weight) + (4.7 * height) - (4.7 * age));
+        gender = "Female"
+        bmr = Math.round((655.1 + (4.35 * weight) + (4.7 * height) - (4.7 * age))* activity_dict[activity]);
     }
+
+    if (isNaN(bmr)) {
+        alert('Please check the parameters you have entered and try again');
+        return false;
+      }
 
     weight_loss = Math.round((bmr/100)*85)
     weight_gain = Math.round((bmr/100)*115)
@@ -22,9 +40,9 @@ function calculateBMR() {
     resp = `<h3>Your details:</h3>
     <p>Gender: ${gender}, Age: ${age}, Weight: ${weight}, Height: ${height}, Activity: ${activity}</p>
     <h3>Your results:</h3>
-    <p>BMR = ${bmr} calories</p>
+    <p>BMR with activity multiplication = ${bmr} calories</p>
     <p>BMR for weight loss: ${weight_loss} calories</p>
     <p>BMR for weight gain: ${weight_gain} calories</p><br>`;
 
-    document.getElementById("demo").innerHTML = resp;
+    document.getElementById("results").innerHTML = resp;
 }
